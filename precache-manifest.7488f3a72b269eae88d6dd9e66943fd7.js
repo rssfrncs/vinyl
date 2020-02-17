ad3ff057c76a656d51a592913c28cabb2e04619d
rssfrncs <rssfrncs@gmail.com>
2020-02-17 21:15:12 +0000

    Updates

---
+++ precache-manifest.7488f3a72b269eae88d6dd9e66943fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585a1891676d67f2da790cde830632c0",
+    "url": "/vinyl/index.html"
+  },
+  {
+    "revision": "406ecc886d5b458f6fb7",
+    "url": "/vinyl/static/css/main.4503ab23.chunk.css"
+  },
+  {
+    "revision": "5495948ae592eae2c0ce",
+    "url": "/vinyl/static/js/2.4d694ef1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/vinyl/static/js/2.4d694ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406ecc886d5b458f6fb7",
+    "url": "/vinyl/static/js/main.f05f93f5.chunk.js"
+  },
+  {
+    "revision": "d4b3018b1ac678420b1d",
+    "url": "/vinyl/static/js/runtime-main.e0633749.js"
+  }
+]);
